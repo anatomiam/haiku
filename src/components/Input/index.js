481086e9c667a1f43a95cc88eabe3146c4f9df49
Eric Dulcet <etdulcet@gmail.com>
2017-01-13 18:43:05 -0500

    axios is submitting post data

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Input extends Component {
     constructor() {
@@ -9,6 +10,7 @@ class Input extends Component {
             line_3: '5'
         };
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
@@ -16,17 +18,30 @@ class Input extends Component {
         this.setState({[event.target.name]: event.target.value});
     }
 
-    // <a href="#"><p className="full-circle"></p></a>
+    handleSubmit(event) {
+        console.log(event);
+        axios.post('/', {
+            line_1: this.state.line_1,
+            line_2: this.state.line_2,
+            line_3: this.state.line_3
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            }
 
     render() {
         console.log("hello");
         return (
-           <form className="input-form" method="post" action="/">
+           <div className="input-form">
                 <input type="text" className="textbox" name="line_1" value={this.state.line_1} onChange={this.handleChange} autoFocus />
                 <input type="text" className="textbox" name="line_2" value={this.state.line_2} onChange={this.handleChange} autoFocus />
                 <input type="text" className="textbox" name="line_3" value={this.state.line_3} onChange={this.handleChange} autoFocus />
-                <input type="submit" value="submit" />
-            </form>
+                <a href="#" onClick={this.handleSubmit}><p className="full-circle"></p></a>
+            </div>
        )
     }
 }
